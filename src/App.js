d293cf9d2da50ea291d838febd7ca21a2eae69f7
Lucas <lucasdelvillar14@gmail.com>
2024-02-21 22:30:01 -0800

    Removed routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
-
-import Home from './pages/home';
-import About from './pages/about';
-import Tutorials from './pages/tutorials';
-import Members from './pages/members';
-import Events from './pages/events';
-// import ContactForm from './pages/contact-form';
+import NavBar from './components/navbar';
+// import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      {/* Router for multi page website */}
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/tutorials' element={<Tutorials />} />
-          <Route path='/members' element={<Members />} />
-          <Route path='/events' element={<Events />} />
-          {/* <Route path='/contact-form' element={<ContactForm />} /> */}
-        </Routes>
-      </Router>
+      <NavBar />
+      
     </div>
   );
 }

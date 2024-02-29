cbb4d4001066464306bc82b7589166d4c33a3dd3
Lucas <lucasdelvillar14@gmail.com>
2024-02-29 15:59:42 -0800

    Add arrow svg

---
+++ src/components/helper-components/ArrowSVG.js
@@ -0,0 +1,22 @@
+const SVGIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      width="24"
+      height="24"
+      className="text-black"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14M12 5l7 7-7 7"
+      />
+    </svg>
+  );
+};
+
+export default SVGIcon;

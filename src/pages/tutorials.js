db11cab719c4db0fa0a04415fbf8e08716fae184
Lucas <lucasdelvillar14@gmail.com>
2024-02-29 12:41:25 -0800

    Add stuff

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -16,7 +16,10 @@ const Tutorials = () => {
                 <div className='header-right'>
                     <h1 className='hr-title'>Tutorials</h1>
                     <p className='hr-intro'>
-                        Welcome to our comprehensive golfing tutorials hub, where passion meets precision! Whether you're a novice looking to take your first swing or a seasoned golfer aiming to refine your technique, our curated collection of tutorials offers a wealth of knowledge to elevate your game. Dive into expert insights on perfecting your swing, mastering various shots, understanding course strategy, and fine-tuning your overall golfing prowess. Our user-friendly tutorials cater to golfers of all levels, providing step-by-step guidance, pro tips, and interactive resources to enhance every aspect of your golfing journey. Discover the keys to unlocking your full potential on the greens and transform your golfing experience into a fulfilling and enjoyable pursuit. Let the learning begin, and may your golf game reach new heights with our dedicated tutorials!
+                        Welcome to our comprehensive golfing tutorials hub, where passion meets precision! Whether you're a novice looking to take your first swing or a seasoned golfer aiming to refine your technique, our curated collection of tutorials offers a wealth of knowledge to elevate your game. 
+                    </p>
+                    <p className='hr-intro hr-intro-second'>
+                        Dive into expert insights on perfecting your swing, mastering various shots, understanding course strategy, and fine-tuning your overall golfing prowess!
                     </p>
                 </div>
             </header>

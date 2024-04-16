1683cdddf15fa0acdee1c7e7cd84d5eace9086e1
Lucas <lucasdelvillar14@gmail.com>
2024-04-16 09:49:54 -0700

    Minor text update

--- src/components/events.js
+++ src/components/events.js
@@ -24,7 +24,7 @@ const Events = () => {
                         </p>
                     </div>
                     <button type="button" className="h-10 w-50 text-black bg-white font-medium hover:bg-gray-400 focus:ring-4 focus:outline-none focus:green-neon text-sm rounded-full px-4 py-2 text-center"> {/*flex flex-row justify-center items-center*/}
-                        Learn More
+                        Instagram
                     {/* <ArrowSVG />  */}
                     </button> 
                 </div>

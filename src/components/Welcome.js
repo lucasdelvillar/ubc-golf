bd854bc49e025490ddba2cb4807a544e168b1365
Lucas <lucasdelvillar14@gmail.com>
2024-04-29 08:35:52 -0700

    Add compressed image w/ black background

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import ArrowSVG from './helper-components/ArrowSVG';
-import glove from '../assets/glove-and-ball-br-min.png';
+import glove from '../assets/untitled_design_2.png';
 import friends from '../assets/tutorials_image-1.jpg';
 
 const Welcome = () => {
